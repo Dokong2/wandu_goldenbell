98f9c72e1d7500d36e96527fa9b58c634ab66b3c
Dukong <jiwan6942@gmail.com>
2024-02-19 18:08:03 +0900

    커뮤니케이션 서버 코드 초반 제작

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
 import threading
 import socket
+chatserver = None
+comserver = None
+conn = None
+addr = None
 # made by wandukong
 # ver 1.0
 
+
+def comsee():
+    global comserver, conn, addr
+    print("com 서버 클라이언트 접속 대기중입니다..")
+    comserver.listen(2)
+    while True:
+        comserver.accept(conn,addr)
+        print("com 서버에 누군가 접속했습니다. 지금 로그인을 대기 중입니다...")
+
+        
+
 def serveropen():
+    global chatserver
+    global comserver
     print("완두완두 골든밸")
+    print("")
     print("개인 게임 : 추후 멀티존에 추가됩니다.")
     print("설치 버전 : 1.0")
     print("서버를 오픈하시려면 [start]를 입력하세요.")
@@ -19,13 +37,20 @@ def serveropen():
             print("서버 매뉴얼은 이것으로 끝입니다.")
         elif start == "start":
             print("서버 실행중...")
-            print("서버 소켓을 생성합니다...")
-            print("1번 포트를 입력하세요.")
+            print("채팅 서버를 생성합니다...")
+            print("채팅 서버 포트를 입력하세요.")
             port = input(">>")
             if port == "":
                 port = 1010
-            goldenbellserver = socket.socket
-            goldenbellserver.bind("127.0.0.1",)
-            print("채팅 서버이 생성되었습니다. 포트 : 8080")
-
-
+            chatserver = socket.socket
+            chatserver.bind("127.0.0.1",int(port))
+            print("채팅 서버이 생성되었습니다. 포트 : " + str(port))
+            print("통신 서버를 생성합니다...")
+            print("통신 서버 포트를 입력하세요.")
+            port = input(">>")
+            if port == "":
+                port = 8080
+            comserver = socket.socket
+            comserver.bind("127.0.0.1", int(port))
+            print("모든 서버가 생성됐습니다.")
+            
