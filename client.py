1e88aaf6eaedd5231e2a07af9c7d6ad1c11932ba
Dukong <jiwan6942@gmail.com>
2024-02-21 22:36:28 +0900

    응 꿀잠자야지~ 오예

--- client.py
+++ client.py
@@ -1,40 +1,42 @@
 import socket
 import threading
 import time
+version = "4.5"
 
-def timeupdate():
-    global now
-    now = time
-
-def nowtime():
-    global now
-    return now.localtime().tm_hour + " : " + now.localtime().tm_min + " : " + now.localtime().tm_sec + " "
+def start():
+    print("완두 골든밸 : 문제를 맞추자!")
+    print("설치 버전 : " + version)
+    print("서버에 로그인하려면 [login]을 입력하세요.")
+    print("명령어 모음을 보시려면 [cmdlist]를 입력하세요.")
+    startcmd = input(">>")
+    if startcmd == "login":
+        login()
+    elif startcmd == "cmdlist":
+        cmdlist()
+        
+def cmdlist():
+    print("버전 [" + version + "] 기준 명령어 리스트입니다.")
+    print("[login] : 서버에 로그인합니다.")
+    print("[cmdlist] : 명령어 리스트를 봅니다.")
 
 def login():
-    print("서버 연결을 시작합니다.")
-    print("연결할 서버의 포트를 입력해주세요.")
-    host = "127.0.0.1"
-    port = int(input(">>"))
-    login(host, port)
-    print("사용할 이름을 입력해주세요.")
+    print("로그인을 시작합니다.")
+    print("닉네임을 입력하세요.")
     name = input(">>")
-    print("이름이 입력되었습니다.")
-    print("서버에 연결을 시작합니다.")
-    client = socket.socket()
-    client.bind(host, port)
-    print("접속 돼었습니다.")
-    print("로그인 정보를 서버에게 보내는 중..")
-    client.send("login " + name.encode())
-    print("정보가 보내졌습니다.")
-    print("서버에게 로그인 성공 데이터를 받기를 대기중입니다..")
-    loginokdata = client.recv(1024).decode()
-    print("서버 내용 : " + loginokdata)
-    if loginokdata == "ok":
-        print("로그인이 성공되었습니다.")
-    else:
-        print("로그인에 실패했습니다. 로그인 데이터가 충돌 된 것 같습니다.")
-        print("서버에게 서버 재부팅 명령을 보내고 있습니다.")
-        client
+    print("통신 서버의 포트를 입력하세요.")
+    host = socket.gethostname()
+    port = int(input(">>"))
+    print("로그인 서버의 포트를 입력하세요.")
+    port2 = int(input(">>"))
+    com_socket = socket.socket()
+    login_socket = socket.socket()
+    com_socket.connect((host, port))
+    login_socket.connect((host, port2))
+    print("로그인을 시작합니다.")
+    print("로그인 중...")
+    login_socket.send("login " + name.encode())
+    print("로그인 데이터를 전송했습니다.")
+    print("로그인이 완료되었습니다.")
 
 if __name__ == "__main__":
-    login()
+    start()
--- server.py
+++ server.py
@@ -1,30 +1,24 @@
 import threading
 import socket
 import time
-now = None
 brodoserver = None
 comlist = {}
 # made by wandukong
-# 스플연어런잼있다스플연어런잼있다스플연어런잼있다스플연어런잼있다스플연어런잼있다스플연어런잼있다
-def timeupdate():
-    global now
-    now = time
-
-def nowtime():
-    global now
-    return now.localtime().tm_hour + " : " + now.localtime().tm_min + " : " + now.localtime().tm_sec + " "
+# 응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~응 꿀잠~
 
 def clientlogin():
     global brodoserver
     brodoserver.listen(2)
     while True:
         conn, addr = brodoserver.accept()
-        print(nowtime + "클라이언트가 접속했습니다.")
+        print("클라이언트가 접속했습니다.")
         print("콘 : " + str(conn))
         print("어드레스 : " + str(addr))
-        print(nowtime + "콘 정보를 데이터에 저장합니다.")
+        print("콘 정보를 데이터에 저장합니다.")
         logindata = conn.recv(1024).decode
-        print("사용자가 로그인 됐습니다. ")
+        name = logindata[6:]
+        print("사용자가 로그인 됐습니다. 닉네임 : " + name)
+
 
 def serveropen():
     global brodoserver
@@ -37,12 +31,11 @@ def serveropen():
     port2 = int(input(">>"))
     loginserver = socket.socket()
     loginserver.bind((host, port))
-    print(nowtime + "로그인 서버가 생성됐습니다.")
+    print("로그인 서버가 생성됐습니다.")
     brodoserver = socket.socket()
     brodoserver.bind((host, port2))
     print("브로도캐스트 서버가 생성됐습니다.")
-    print(nowtime + "클라이언트 대기를 시작합니다...")
+    print("클라이언트 대기를 시작합니다...")
 
 if __name__ == "__main__":
-    threading.Thread(target=timeupdate).start
     serveropen()
